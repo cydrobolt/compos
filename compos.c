d5687c336ac17829912a8d84cb6375d56a07f008
Chaoyi Zha <summermontreal@gmail.com>
2016-12-14 17:47:43 -0500

    Set up netfilter to intercept packets

--- compos.c
+++ compos.c
@@ -1,18 +1,57 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/init.h>
+#include <linux/list.h>
+#include <asm/uaccess.h>
+
+// Import proc_fs and netfilter
+#include <linux/proc_fs.h>
+#include <linux/tcp.h>
+#include <linux/netfilter.h>
+#include <linux/netfilter_ipv4.h>
+#include <linux/ip.h>
+#include <linux/skbuff.h>
 
 MODULE_LICENSE("MIT");
 MODULE_AUTHOR("cydrobolt");
 MODULE_DESCRIPTION("compos self-control module");
 
+static struct nf_hook_ops nfho;
+
+unsigned int hook_func_out (void *priv,
+                            struct sk_buff *skb,
+                            const struct nf_hook_state *state) {
+
+    // Intercept outbound packets
+    struct iphdr *ip_header = (struct iphdr *)skb_network_header(skb);
+    // unsigned int src_ip = (unsigned int)ip_header->saddr;
+    unsigned int dest_ip = (unsigned int)ip_header->daddr;
+
+    printk(KERN_INFO "packet with dest %u", dest_ip);
+    if (false) {
+        return NF_DROP;
+    }
+    else {
+        return NF_ACCEPT;
+    }
+}
+
 static int __init compos_init(void) {
     printk(KERN_INFO "Loading compos\n");
+
+    nfho.hook = hook_func_out;
+    nfho.hooknum = NF_INET_LOCAL_OUT;
+    nfho.pf = PF_INET;
+    nfho.priority = NF_IP_PRI_FIRST;
+    // Register hook
+    nf_register_hook(&nfho);
+
     return 0;
 }
 
 static void __exit compos_cleanup(void) {
     printk(KERN_ALERT "Cleaning up compos.\n");
+    nf_unregister_hook(&nfho);
 }
 
 module_init(compos_init);
