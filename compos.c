cfee955b51b5b0fa84f3ef1a3943655c64c2d1d2
Chaoyi Zha <summermontreal@gmail.com>
2016-12-15 16:33:38 -0500

    Use proc_create instead of <3.10 syntax

--- compos.c
+++ compos.c
@@ -6,6 +6,7 @@
 
 // Import proc_fs and netfilter
 #include <linux/proc_fs.h>
+#include <linux/seq_file.h>
 #include <linux/tcp.h>
 #include <linux/netfilter.h>
 #include <linux/netfilter_ipv4.h>
@@ -15,12 +16,63 @@
 // Import JSON libraries to interface with compos_ctl
 #include "lib/jsmn.h"
 
+// Define constants
+#define PROCFS_NAME         "compos"
+#define PROCFS_MAX_SIZE     1024
 
 MODULE_LICENSE("MIT");
 MODULE_AUTHOR("cydrobolt");
 MODULE_DESCRIPTION("compos self-control module");
 
 static struct nf_hook_ops nfho;
+static struct proc_dir_entry *proc_entry;
+static unsigned long procfs_buffer_size = 0;
+static char procfs_buffer[PROCFS_MAX_SIZE];
+
+static ssize_t cp_proc_read(struct file*, char*, size_t, loff_t*);
+static ssize_t cp_proc_write(struct file*, char*, size_t, loff_t*);
+
+
+static const struct file_operations cp_fops = {
+    owner: THIS_MODULE,
+    // .open       = cp_proc_open,
+    read: cp_proc_read,
+    write: cp_proc_write
+};
+
+static ssize_t cp_proc_write(struct file *file, char *buffer, size_t count, loff_t *offset) {
+    procfs_buffer_size = count;
+    if (procfs_buffer_size > PROCFS_MAX_SIZE) {
+        procfs_buffer_size = PROCFS_MAX_SIZE;
+    }
+
+    // write data to buffer
+    if ( copy_from_user(procfs_buffer, buffer, procfs_buffer_size) ) {
+        // debug data
+        // printk(KERN_INFO procfs_buffer);
+        printk(KERN_INFO "received information in procfile");
+        return -EFAULT;
+    }
+
+    return procfs_buffer_size;
+}
+
+static ssize_t cp_proc_read(struct file *file, char *buffer, size_t count, loff_t *offset) {
+    static int finished = 0;
+    int ret = 0;
+
+    printk(KERN_INFO "procfile_read (/proc/%s) called\n", PROCFS_NAME);
+
+    if (finished) {
+        printk(KERN_INFO "procfs_read: END\n");
+        finished = 0;
+        return 0;
+    }
+
+    finished = 1;
+    ret = sprintf(buffer, "Hello,world!\n");
+    return ret;
+}
 
 unsigned int hook_func_out (void *priv,
                             struct sk_buff *skb,
@@ -49,12 +101,24 @@ static int __init compos_init(void) {
     // Register hook
     nf_register_hook(&nfho);
 
+    // Initialize proc file
+    proc_entry = proc_create(PROCFS_NAME, 0, NULL, &cp_fops);
+
+    if (proc_entry == NULL) {
+        remove_proc_entry(PROCFS_NAME, NULL);
+        printk(KERN_ALERT "Could not initialize /proc/%s\n", PROCFS_NAME);
+        return -ENOMEM;
+    }
+
+    printk(KERN_INFO "compos proc file created");
+
     return 0;
 }
 
 static void __exit compos_cleanup(void) {
     printk(KERN_ALERT "Cleaning up compos.\n");
     nf_unregister_hook(&nfho);
+    remove_proc_entry(PROCFS_NAME, NULL);
 }
 
 module_init(compos_init);
